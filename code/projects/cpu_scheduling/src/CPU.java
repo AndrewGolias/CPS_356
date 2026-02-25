5d9ea8b355076a03b50c2f4d5571b20c24e9ff88
Andrew Golias <agolias53@gmail.com>
2026-02-24 22:00:02 -0500

    preemptive sjf implemented

--- code/projects/cpu_scheduling/src/CPU.java
+++ code/projects/cpu_scheduling/src/CPU.java
@@ -9,25 +9,29 @@ public class CPU {
         // sort by arrival time
         // processes.sort(Comparator.comparingInt(Process::getArrivalTime));
         processes.sort((p1, p2) -> p1.getArrivalTime() - p2.getArrivalTime());
+        
         System.out.println("\tProcesses sorted by arrival time (FCFS): " + processes);
+        
         for(Process p : processes) {
-            while(time < p.getArrivalTime()) {
-                System.out.printf("Time: %d   ... CPU is idling\n", time);
-                time++;
+            while(this.time < p.getArrivalTime()) {
+                System.out.printf("Time: %d   ... CPU is idling\n", this.time);
+                this.time++;
             }
-            p.run(time);
-            time += p.getBurstTime();
+            p.run(this.time);
+            this.time += p.getBurstTime();
         }
+        System.out.printf("Time: %d   ... CPU is idling\n", this.time);
     }
 
     public void sjf_scheduling(List<Process> processes, int mode) {
         // sort by burst time
-        List<Process> executionOrder = new ArrayList<>();
         int completedProcesses = 0;
+        int size = processes.size();
 
         switch(mode) {
             case 0:         // non-preemtive
-                while(completedProcesses < processes.size()) {
+                List<Process> executionOrder = new ArrayList<>();
+                while(completedProcesses < size) {
                     Process currShortestProcess = null;
                     
                     // find shortest job currently in waiting queue
@@ -38,23 +42,51 @@ public class CPU {
                         }
                     }
 
+                    // run process OR CPU idles if no process is available
                     if(currShortestProcess != null) {
-                        currShortestProcess.run(time);
-                        time += currShortestProcess.getBurstTime();
+                        currShortestProcess.run(this.time);
+                        this.time += currShortestProcess.getBurstTime();
                         executionOrder.add(currShortestProcess);
+                        processes.remove(currShortestProcess);
                         completedProcesses++;
                     } else {
-                        System.out.printf("Time: %d   ... CPU is idling\n", time);
-                        time++;
+                        System.out.printf("Time: %d   ... CPU is idling\n", this.time);
+                        this.time++;
                     }
                 }
                 processes = executionOrder;
+                System.out.println("\tProcesses sorted by burst time (SJF): " + processes);
                 break;
-            case 1:     // preemtive
 
+            case 1:     // preemtive
+                while(completedProcesses < size) {
+                    Process currentProcess = null;
+                    int minRemainingTime = Integer.MAX_VALUE;
+
+                    // find process with shortest remaining time that has already arrived
+                    for(Process p : processes) {
+                        if(p.getArrivalTime() <= this.time && !p.isFinished()
+                                && p.getRemainingTime() < minRemainingTime) {
+                            minRemainingTime = p.getRemainingTime();
+                            currentProcess = p;
+                        }
+                    }
+
+                    // run a single time unit
+                    if(currentProcess != null) {
+                        currentProcess.run(this.time);
+                        if(currentProcess.isFinished()) {
+                            completedProcesses++;
+                            int turnaroundTime = this.time - currentProcess.getArrivalTime();
+                            currentProcess.setWaitingTime(turnaroundTime - currentProcess.getBurstTime());
+                        }
+                    } else {
+                        System.out.printf("Time: %d   ... CPU is idling\n", this.time);
+                    }
+                    this.time++;
+                }
                 break;
         }
-        System.out.println("\tProcesses sorted by burst time (SJF): " + processes);
     }
 
     public static void main(String[] args) {
@@ -69,6 +101,16 @@ public class CPU {
         // processes.add(new Process(6, 16, 2));
         // processes.add(new Process(7, 10, 1));
 
+
+        // processes.add(new Process(5, 15, 5));
+		// processes.add(new Process(6, 16, 2));
+		// processes.add(new Process(4, 8, 3));
+		// processes.add(new Process(7, 3, 1));
+		// processes.add(new Process(2, 3, 2)); //P2
+		// processes.add(new Process(1, 2, 4)); //P1
+		// processes.add(new Process(3, 3, 1)); //P3
+        
+        
         processes.add(new Process(1,0,7));
         processes.add(new Process(2,2,4));
         processes.add(new Process(3,4,1));
@@ -78,7 +120,7 @@ public class CPU {
         // create CPU to execute process
         CPU cpu = new CPU();
         // cpu.fcfs_scheduling(processes);
-        cpu.sjf_scheduling(processes, 0);   // non-preemptive
+        cpu.sjf_scheduling(processes, 1);   // non-preemptive
         //cpu.fcfs_scheduling(processes);
         System.out.println();
     }
--- code/projects/cpu_scheduling/src/Process.java
+++ code/projects/cpu_scheduling/src/Process.java
@@ -3,14 +3,15 @@ public class Process {
     int arrTime;        // time arrived
     int burstTime;      // time needed for CPU service
     int remainingTime;  // needed for preemptive mode
-    private boolean jobCompleted;
+    int waitingTime;
+    private boolean finished;
 
     public Process(int id, int arrTime, int burstTime) {
         this.id = id;
         this.arrTime = arrTime;
         this.burstTime = burstTime;
         this.remainingTime = burstTime;
-        this.jobCompleted = false;
+        this.finished = false;
     }
 
     /*** helper getter method -> returns process arrival time */
@@ -19,7 +20,14 @@ public class Process {
     /*** helper getter method -> returns process burst time */
     public int getBurstTime() { return burstTime; }
 
-    public boolean isFinished() { return jobCompleted; }
+    /*** helper getter method -> returns process remaining time */
+    public int getRemainingTime() { return remainingTime; }
+
+    public boolean isFinished() { return finished; }
+
+    public void setWaitingTime(int waitingTime) {
+        this.waitingTime = waitingTime;
+    }
 
     /**
      * runs the process and gives the global current time
@@ -27,13 +35,10 @@ public class Process {
      * @param currTime - the global time of the CPU when executing process
      */
     public void run(int currTime) {
-        int timer=0;
-        while(timer < burstTime) {
-            System.out.printf("Time: %d   Process P%d is running...\n", currTime, this.id);
-            timer++;
-            currTime++;
-        }
-        jobCompleted = true;
+        System.out.printf("Time: %d   Process P%d is running...\n", currTime, this.id);
+        remainingTime--;
+        if(remainingTime == 0)
+            finished = true;
     }
 
     @Override
